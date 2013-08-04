8bdb262bd72b2bdfa8f0aca14155ba252d2bf2c0
afeique <afeique@gmail.com>
2013-08-04 16:14:21 -0400

    reorganized files, edited paths

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+define('ROOT', dirname(dirname(__DIR__) ) .'/' );
+define('VIEWS', ROOT .'views/');
+define('LAYOUTS', VIEWS .'layouts/');
+
+require_once ROOT .'vendor/autoload.php';
+$klein = new \Klein\Klein();
+
+$klein->respond('[/]?[:page]?', function ($request, $response, $service) {
+	// Header & Footer
+	$service->layout(LAYOUTS .'main.phtml');
+	
+	// Page-checking
+	switch($request->page) {
+		case 'upload': 
+		case '':
+			$service->render(VIEWS .'upload.phtml');
+		break;
+		default:
+			$service->render(VIEWS .'404.phtml');
+		break;
+		
+	}
+});
+
+$klein->dispatch();
+
+?>
