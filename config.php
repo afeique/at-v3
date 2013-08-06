363f1ba3dc2f34710a599e5982a0cbc08552ce01
afeique <afeique@gmail.com>
2013-08-06 18:51:06 -0400

    Created custom signin/signout pages
    Created a new layout and view for redirecting users with a message -- identical to default IPB redirect.
    Created router pages for signing in and signing out users and offering them helpful messages.
    No longer have to worry about auth_key.

--- config.php
+++ config.php
@@ -6,3 +6,5 @@ define('VIEW_DIR', ROOT_DIR .'views/');
 define('LAYOUT_DIR', VIEW_DIR .'layouts/');
 
 define('VIEW_EXT', '.phtml');
+
+define('ROOT_URL', "http://$_SERVER[HTTP_HOST]");
--- public/index.php
+++ public/index.php
@@ -3,17 +3,71 @@
 require '../config.php';
 require LIB_DIR .'functions.php';
 require ROOT_DIR .'vendor/autoload.php';
-require LIB_DIR .'ipbwi/ipbwi.inc.php';
+require LIB_DIR .'ipbwi/ipbwi.inc.php'; // instantiates $ipbwi class
 
 $klein = new \Klein\Klein();
 
+// applies to all responses
 $klein->respond(function ($request, $response, $service, $app) use ($ipbwi) {
+	// make ipbwi available to all responses
 	$service->ipbwi = $ipbwi;
+
+	// default Header & Footer
+	$service->layout( \AcrossTime\layout('main') );
 });
 
-$klein->respond('[/]?[:page]?', function ($request, $response, $service) {
-	// Header & Footer
-	$service->layout( \AcrossTime\layout('main') );
+$klein->respond('/signin', function ($request, $response, $service) {
+	$service->layout( \AcrossTime\layout('redirect') );
+
+	$rememberMe = False;
+	if (isset($_POST['rememberMe']))
+		$rememberMe = True;
+
+	$anonymous = False;
+	if (isset($_POST['anonymous']))
+		$anonymous = True;
+
+	$signinSuccess = $service->ipbwi->member->login($_POST['username'], $_POST['password'], $rememberMe, $anonymous);
+
+	$service->error = False;
+	if ($signinSuccess and $service->ipbwi->member->isLoggedIn()) {
+		$service->message = 'Successfully signed in!';
+	} else if (!$signinSuccess and $service->ipbwi->member->isLoggedIn()) {
+		$service->message = 'Already signed in!';
+	} else {
+		$service->message = 'Failed to sign in.';
+		$service->error = True;
+	}
+
+	$service->redirect = ROOT_URL;
+	if (isset($_POST['redirect']))
+		$service->redirect = $_POST['redirect'];
+
+	$service->render( \AcrossTime\view('redirect-message') );
+});
+
+$klein->respond('/signout', function ($request, $response, $service) {
+	$service->layout( \AcrossTime\layout('redirect') );
+
+	$loggedIn = $service->ipbwi->member->isLoggedIn();
+
+	$service->error = False;
+	if (!$loggedIn) {
+		$service->message = 'Not signed in.';
+		$service->error = True;
+	} else if ($loggedIn) {
+		$service->ipbwi->member->logout();
+		$service->message = 'Signed out!';
+	}
+
+	$service->redirect = ROOT_URL;
+	if (isset($_GET['redirect']))
+		$service->redirect = urldecode($_GET['redirect']);
+	
+	$service->render( \AcrossTime\view('redirect-message') );
+});
+
+$klein->respond('/', function ($request, $response, $service) {
 	
 	// Page-checking
 	switch($request->page) {
