859e57ad5f6986b6d124e638857a46915ee5712c
IllyaMoskvin <imoskvin@indiana.edu>
2013-08-09 06:44:44 -0400

    Task Timer & Timeline GET
    - Abstracted /lib/ipbwi/config.inc.php a bit (was not working on my
    server)
    - Added /views/timeline.phtml; updated main navbar links to match.
    - Set up correct page routing in index.php: [/]?[:page] & routed
    'timeline'
    - Added /public/get/timeline.php, for retrieving SQL data for timeline
    - TODO: move <scripts> into head.js() and test existing code
    - TODO: work on Time Task - ng-model form into text above timer
    - TODO: integrate Isotope into timeline splash

--- config.php
+++ config.php
@@ -4,7 +4,9 @@ define('ROOT_DIR', __DIR__ .'/' );
 define('LIB_DIR', ROOT_DIR .'lib/');
 define('VIEW_DIR', ROOT_DIR .'views/');
 define('LAYOUT_DIR', VIEW_DIR .'layouts/');
+define('FORUM_DIR', ROOT_DIR .'/public/forums/');
 
 define('VIEW_EXT', '.phtml');
 
-define('ROOT_URL', "http://$_SERVER[HTTP_HOST]");
+define('ROOT_URL', "http://$_SERVER[HTTP_HOST]"); // Should we add trailing slash?
+
